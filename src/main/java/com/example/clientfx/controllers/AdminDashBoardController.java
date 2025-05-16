d620b83361a6e2756c77dfb588002453d75f0e6b
tim569 <timothyobrike@gmail.com>
2025-05-16 08:46:08 +0100

    lets start from scratch lol

---
+++ src/main/java/com/example/clientfx/controllers/AdminDashBoardController.java
@@ -0,0 +1,20 @@
+package com.example.clientfx.controllers;
+
+
+// here's the idea
+// this has a dashboard to create the new Users, 
+// list the Users
+
+// Under each listed user, onclick(select) on them will lead you to a new window
+// that window is for you to Delete User or Edit User or Login as User
+
+// This is for the spawned new Chat window for a particular user
+// If Login as User, provide a list of all the available chats for user in a new Chat window
+// the window has a side bar that has the other chats aswell as the current chat being viewed
+// in this chat window, you can send messages delete messages, and edit messages
+
+
+
+public class AdminDashboardController {
+    
+}
